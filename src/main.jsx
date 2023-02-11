41d95efc1930993c888ab37ed2f1148ec8952278
suryansh9315 <suryansh.sharma9315@gmail.com>
2023-02-11 12:02:18 +0530

    Initialized frontend with tailwind css

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,7 @@ import App from "./App";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "./styles/globals.css";
 
-// This is the chainId your dApp will work on.
-const activeChainId = ChainId.Mainnet;
+const activeChainId = ChainId.Goerli;
 
 const container = document.getElementById("root");
 const root = createRoot(container);
